01dfe600996da96d8636bcde0854cc41d75ad821
Endijs Vanags <endijs.vanags@gmail.com>
2025-08-31 14:07:31 +0000

    Import please this Firebase SDK
    // Import the functions you need from t

---
+++ src/lib/firebase.ts
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp, getApps, getApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBxp2j0gZf-sIrOrOMTHnuo6RsohVTiwcQ",
+  authDomain: "business-cards-59f22.firebaseapp.com",
+  projectId: "business-cards-59f22",
+  storageBucket: "business-cards-59f22.appspot.com",
+  messagingSenderId: "935421321413",
+  appId: "1:935421321413:web:df472029ebe1b0209cd0ac"
+};
+
+// Initialize Firebase
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+
+export { app };
